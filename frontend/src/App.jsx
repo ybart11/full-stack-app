445ece4cba29d49714ab168ca8cdab84a2fa664d
ybart11 <yovany1061@gmail.com>
2024-09-17 10:52:50 -0400

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
   return (
     <>
       <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
-      <button onClick={openCreateModal}>Create New Contact</button>
+      <button onClick={openCreateModal}>Create Contact</button>
       {isModalOpen && <div className="modal">
         <div className="modal-content">
           <span className="close" onClick={closeModal}>&times;</span>
